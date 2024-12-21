c22de251fe61b1c0e9d359166f47530b2a9dd3ad
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-21 18:10:41 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ export default function App() {
         Settext(event.target.value)
     }
     return (
-        <div className='mark1'>
+        <div className='app'>
             <div className='preview'>
             <h1>Heading</h1><br></br><br></br><span>This is some <strong>bold</strong> text.</span>  
              <textarea onChange={text}>
